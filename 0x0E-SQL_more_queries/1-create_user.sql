1c0c899e6e63e388c10fecd53948eabc6fa88a1b
Ayoub-kf7 <ayoub98koufi@gmail.com>
2023-08-16 17:34:08 -0700

    update

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
--- CREAT user
-CREATE USER If not exists 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- script that creates the MySQL server user user_0d_1
+
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
