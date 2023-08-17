d7ff38e51fe7ea898b6aed3f1ecc2fc98d57241b
Ayoub-kf7 <ayoub98koufi@gmail.com>
2023-08-16 17:29:19 -0700

    update

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
--- give some permission to our new user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-Grant ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- CREAT user
+CREATE USER If not exists 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
